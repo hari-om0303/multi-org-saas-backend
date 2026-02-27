8a5a1cf5a0930ce5ef3fb83c8a30564d82522330
Hari Om Gupta <hariomgupta0303@gmail.com>
2026-02-27 18:46:03 +0530

    Connect Routes to App

--- src/app.js
+++ src/app.js
@@ -1,13 +1,17 @@
 //Express App
 const express = require('express');
 const cors = require('cors');
+const authRoutes = require('./modules/auth/auth.routes');
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+
 app.get('/health', (req,res)=>{
     res.json({status: 'ok'});
 })
+
+app.use('/api/auth', authRoutes);
 
 module.exports = app;
