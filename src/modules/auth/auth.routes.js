685530b900a7a99a10ee98dab5dba5d5a5458e63
Hari Om Gupta <hariomgupta0303@gmail.com>
2026-02-28 11:28:00 +0530

    added login routes

--- src/modules/auth/auth.routes.js
+++ src/modules/auth/auth.routes.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { register } = require("./auth.controller");
+const { register , login } = require("./auth.controller");
 
 // Create Auth Routes
 router.post("/register", register);
+// add login route
+router.post("/login" , login);
 
 module.exports = router;
