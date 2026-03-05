2b6e75288c6137c140cb04e3956eededbb09889e
Hari Om Gupta <hariomgupta0303@gmail.com>
2026-03-05 18:40:46 +0530

    name changed

---
+++ src/modules/project/project.model.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const projectSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    description: {
+      type: String,
+    },
+
+    orgId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Organization",
+      required: true,
+    },
+
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Project", projectSchema);
