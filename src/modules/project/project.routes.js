ee4470afe7a3ffd35569f2cbcf9dafd93f9c5cc6
Hari Om Gupta <hariomgupta0303@gmail.com>
2026-03-06 19:12:57 +0530

    project controller (POST api done)

--- src/modules/project/project.routes.js
+++ src/modules/project/project.routes.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const authmiddleware = require('../../middlewares/auth.middleware')
+const authMiddleware = require('../../middlewares/auth.middleware')
+const {createProject } = require('./project.controller')
 
-router.get('/', authmiddleware , (req,res)=>{
+
+router.get('/', authMiddleware , (req,res)=>{
     res.json({
         message : "Protected route accessed",
         user : req.user,
     });
 });
+
+router.post("/", authMiddleware, createProject);
 
 module.exports = router;
 
