04e308e951a19c013302f847e338a1b110229a1d
Hari Om Gupta <hariomgupta0303@gmail.com>
2026-03-08 22:25:57 +0530

    getProject route added

--- src/modules/project/project.routes.js
+++ src/modules/project/project.routes.js
@@ -1,17 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const authMiddleware = require('../../middlewares/auth.middleware')
-const {createProject } = require('./project.controller')
+const {createProject , getProjects } = require('./project.controller')
 
 
-router.get('/', authMiddleware , (req,res)=>{
-    res.json({
-        message : "Protected route accessed",
-        user : req.user,
-    });
-});
+// router.get('/', authMiddleware , (req,res)=>{
+//     res.json({
+//         message : "Protected route accessed",
+//         user : req.user,
+//     });
+// });
 
 router.post("/", authMiddleware, createProject);
+router.get("/", authMiddleware, getProjects);
 
 module.exports = router;
 
