e7cc9a29ee889a6a62b25d141badf9fc5988e77c
Hari Om Gupta <hariomgupta0303@gmail.com>
2026-03-15 08:51:35 +0530

    update project controller added

--- src/modules/project/project.controller.js
+++ src/modules/project/project.controller.js
@@ -66,6 +66,48 @@ const getProjectById = async (req, res) => {
   }
 };
 
+
+const updateProject = async (req, res) => {
+  try {
+
+    const { title, description } = req.body;
+
+    const project = await Project.findOne({
+      _id: req.params.id,
+      orgId: req.user.orgId,
+      isActive: true
+    });
+
+    if (!project) {
+      return res.status(404).json({
+        message: "Project not found"
+      });
+    }
+
+    if (
+      req.user.role !== "ORG_ADMIN" &&
+      project.createdBy.toString() !== req.user.userId
+    ) {
+      return res.status(403).json({
+        message: "You are not allowed to update this project"
+      });
+    }
+
+    project.title = title || project.title;
+    project.description = description || project.description;
+
+    await project.save();
+
+    res.json({
+      message: "Project updated successfully",
+      project
+    });
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
 module.exports = {
-  createProject, getProjects , getProjectById
+  createProject, getProjects , getProjectById , updateProject
 };
