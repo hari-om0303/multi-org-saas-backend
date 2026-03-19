2d8e2c428f9c2a91c3dc159427c314e97028d288
Hari Om Gupta <hariomgupta0303@gmail.com>
2026-03-20 01:15:08 +0530

    All Routes (Express app)

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 //Express App
 const express = require('express');
 const cors = require('cors');
+const authMiddleware = require('./middlewares/auth.middleware');
 const authRoutes = require('./modules/auth/auth.routes');
+const projectRoutes = require('./modules/project/project.routes');
+const orgRateLimiter = require('./middlewares/rateLimit.middleware');
 const app = express();
 
 app.use(cors());
@@ -12,7 +15,16 @@ app.get('/health', (req,res)=>{
     res.json({status: 'ok'});
 })
 
+
+// 1. Auth first (sets req.user)
+app.use('/api', authMiddleware);
+
+// Apply Organization-Level Rate Limiting
+app.use('/api', orgRateLimiter);
 // Connect Routes to App
 app.use('/api/auth', authRoutes);
+// Connect Project Routes
+app.use('/api/projects', projectRoutes);
+
 
 module.exports = app;
